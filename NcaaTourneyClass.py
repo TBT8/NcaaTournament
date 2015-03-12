6670b385e6dc4421f2e5bcb89d2b6fb6f61483a5
TBT8 <ttruman8@gmail.com>
2015-03-12 15:12:57 -0400

    Added Additional Class

--- NcaaTourneyClass.py
+++ NcaaTourneyClass.py
@@ -1,4 +1,3 @@
-
 class NcaaGame(object):
     def __init__(self):
         self.region = None
@@ -20,6 +19,10 @@ class NcaaGame(object):
     
     def setSeeds(self,seed1, seed2):
         self.seed1 = seed1
+        self.seed2 = seed2
+    def setSeed1(self, seed1):
+        self.seed1 = seed1
+    def setSeed2(self, seed2):
         self.seed2 = seed2 
     def setAfter(self, after):
         self.after = after
@@ -32,6 +35,22 @@ class NcaaGame(object):
         self.winner = team
     def setPoints(self, points):
         self.points = points
+    def getWinner(self):
+        return self.winner
+    def getBefore1(self):
+        return self.before1
+    def getBefore2(self):
+        return self.before2
+    def getAfter(self):
+        return self.after
+    def getSeed1(self):
+        return self.seed1
+    def getSeed2(self):
+        return self.seed2
+    def getTeam2(self):
+        return self.team2
+    def getTeam1(self):
+        return self.team1
         
     def getData(self):
         print 'team1: ' + str(self.team1) + '\n' +'team2: ' + str(self.team2) +\
@@ -41,12 +60,16 @@ class NcaaGame(object):
         
 class NcaaTournament(object):
     def __init__(self, teams, points = [1,2,4,8,10,20]):
-        self.games = [NcaaGame() for i in range(len(teams)-1)]
-       
-        seed = (i%8)+1
+        self.games = [NcaaGame() for i in range(63)]
+        self.owner = None
+        
+        seeds = [1,16,8,9,5,12,4,13,6,11,3,14,7,10, 2,15]
+        s = 0
         for i in range(32):
-            self.games[i].setSeeds(seed, 17 - seed)
+            s = s%15
+            self.games[i].setSeeds(seeds[s], seeds[s+1])
             self.games[i].setPoints(points[0])
+            s += 2
             if i%2 == 0:
                 self.games[i].setAfter(self.games[i+32])
             else:
@@ -99,6 +122,12 @@ class NcaaTournament(object):
            self.games[i].setTeam2(teams[n+1])
            n += 2
            
+    
+            
+        
+class mainTournament(NcaaTournament):
+        
+             
     def updateGame(self, gameNum, team1, team1score, team2, team2score):
         if team1 != self.games[gameNum].team1 and team1 != self.games[gameNum].team2:
             return "Team 1 is not a valid team for GameID"
@@ -108,21 +137,51 @@ class NcaaTournament(object):
         self.games[gameNum].setResult(team2, team2score)
         self.games[gameNum].setResult(team1, team1score)
         
-        aftGame = self.games[gameNum].after
+        if team1 == self.games[gameNum].getTeam2():
+            temp = team1
+            team1 = team2
+            team2 = temp
+            temp1 = team1score
+            team1score = team2score
+            team2score = temp1
+        
+        aftGame = self.games[gameNum].getAfter()
+        seed = self.games[gameNum].getSeed2()
+        
         if team1score > team2score:
             winner = team1
+            seed = self.games[gameNum].getSeed1()
         else:
             winner = team2
         
         self.games[gameNum].setWinner(winner)
         
-        if self.games[gameNum] == aftGame.before1:
+        if self.games[gameNum] == aftGame.getBefore1():
             aftGame.setTeam1(winner)
+            aftGame.setSeed1(seed)
         else:
             aftGame.setTeam2(winner)
-            
+            aftGame.setSeed2(seed)
+
+class TournamentEntry(NcaaTournament):
+    def setOwner(self, Owner):
+        self.owner = Owner
         
-         
+    def addPicks(self, picks):
+        for i in range(len(self.games)):
+            self.games[i].setWinner(picks[i])
             
+        for i in range(32, 63):
             
-        
+            self.games[i].setTeam1(self.games[i].getBefore1().getWinner())
+            self.games[i].setTeam2(self.games[i].getBefore2().getWinner())
+            
+            if self.games[i].getTeam1() == self.games[i].getBefore1().getTeam1():
+                self.games[i].setSeed1(self.games[i].getBefore1().getSeed1())
+            else:
+                self.games[i].setSeed1(self.games[i].getBefore1().getSeed2())
+            
+            if self.games[i].getTeam2() == self.games[i].getBefore2().getTeam1():
+                self.games[i].setSeed2(self.games[i].getBefore2().getSeed1())
+            else:
+                self.games[i].setSeed2(self.games[i].getBefore2().getSeed2())
